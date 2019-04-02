e1fbb3d4b50b3ec41861a931655add4de2309494
BranK <ItsBranK@users.noreply.github.com>
2019-04-01 22:50:06 -0400

    BranksMod V1.0.1

--- BranksMod/Properties/Settings.Designer.cs
+++ BranksMod/Properties/Settings.Designer.cs
@@ -49,7 +49,7 @@ namespace BranksMod.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool AutoUpdate {
             get {
                 return ((bool)(this["AutoUpdate"]));
